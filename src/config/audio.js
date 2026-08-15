ae6ce5eeb3ed5b3b43831dac3102c435aa4cefb5
ThaoLinn20 <aotrame@gmail.com>
2026-08-15 17:23:14 +0700

    feat: add config for audio

---
+++ src/config/audio.js
@@ -0,0 +1,9 @@
+export const AUDIO_CONFIG = {
+  baseURL: {
+    legacy: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/329180/',
+    new: 'https://shellsound.s3.ap-southeast-2.amazonaws.com/effect/'
+  },
+  physics: {
+    speedOfSound: 343
+  }
+};
